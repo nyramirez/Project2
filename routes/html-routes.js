61c9effcc377b252131f5e60971b4bb86d70c998
Nefas <nyramirez@gmail.com>
2018-12-05 20:27:14 -0600

    user-auth folder deleted

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,23 +9,43 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.render("index.hbs");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../views/signup.hbs"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.render("members.hbs");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../views/index.hbs"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "../views/members.hbs"));
   });
+
+  /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('signup');
+// });
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('members');
+// });
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index');
+// });
 
 };
