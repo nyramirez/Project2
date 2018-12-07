b67c42c928e569ea9e98aff8aa84878bcea47c5e
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-06 19:34:16 -0600

    hbs rendering fixed

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
-const createError = require('http-errors');
-const express = require('express');
-
-var session = require("express-session");
-var passport = require("./config/passport");
-
-
-
-
+// const createError = require('http-errors');
+// const session = require("express-session");
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+const passport = require("./config/passport");
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const PORT = process.env.PORT || 3020;
 
-
-const db = require("./models/");
-const sequelize = require("sequelize");
-
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const membersRouter = require('./routes/members');
-const signUpRouter = require('./routes/signup');
-
+const express = require('express');
 const app = express();
+let PORT = process.env.PORT || 3020;
+require("./routes/html-routes")(app);
+
+const db = require("./models");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+require("./models/user.js");
+require("./config/passport.js");
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/members', membersRouter);
-app.use('/signup', signUpRouter);
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT http://localhost:" + PORT);
+//   });
+// });
 
-
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT http://localhost:" + PORT);
-  });
-});
-
-db.PO.bulkCreate([{purchaseOrder: 5321, contact:"Barbara", customer: "PAN", status: "WIP"}, {purchaseOrder: 78689, contact: "Paul", customer:"WAMP", status: "DELAYED"}, {purchaseOrder: 73489, contact: "Valinda", customer: "ASN", status: "COMPLETE"}])
+/*db.PO.bulkCreate([{purchaseOrder: 5321, contact:"Barbara", customer: "PAN", status: "WIP"}, {purchaseOrder: 78689, contact: "Paul", customer:"WAMP", status: "DELAYED"}, {purchaseOrder: 73489, contact: "Valinda", customer: "ASN", status: "COMPLETE"}])
   .then(data => {
     console.log(data);
     return db.SO.bulkCreate([{salesOrder: "WAM1123", description: "pipe1", orderQty: 500, status: "WIP", purchaseOrder: 5321}, {salesOrder: "KMAN0832", description: "pipe2", orderQty: 160, status: "WIP", purchaseOrder: 5321}, {salesOrder: "RSTO1709", description: "pipe1", orderQty: 200, status: "COMPLETE", purchaseOrder: 73489}]);
@@ -77,6 +61,10 @@ db.PO.bulkCreate([{purchaseOrder: 5321, contact:"Barbara", customer: "PAN", stat
   })
   .catch(err => {
     console.log(err);
+  });*/
+
+   app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT);
   });
 
 module.exports = app;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,25 +1,36 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // if (req.user) {
+    //   res.render("signup.hbs");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup.hbs");
+  });
+
+  app.get("/sign-up", function(req, res) {
+    // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.render("signup.hbs");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup.hbs");
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // if (req.user) {
+    //   res.render("login.hbs");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login.hbs");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
