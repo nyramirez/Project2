9bc3abbcf8ddf6d3027af7e4a30500f0105dce9d
malcolmberry96 <malcolm.berry65@gmail.com>
2018-12-07 13:18:03 -0600

    fixing database

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.render("signup.handlebars");
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", function(req, res) {
@@ -19,13 +19,13 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.render("login.handlebars");
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.render("members.handlebars");
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
 };
