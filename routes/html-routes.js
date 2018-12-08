e1efe52ed1acd560c18fe5de51fa58814b67f4ca
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-08 11:02:01 -0600

    added get route for /manager route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,16 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+let isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-
     app.get("/", function(req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/members");
         }
-        res.render('signup');
+        res.render("signup");
     });
 
     app.get("/login", function(req, res) {
@@ -19,13 +18,16 @@ module.exports = function(app) {
         if (req.user) {
             res.redirect("/members");
         }
-        res.render('login');
+        res.render("login");
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
-        res.render('members');
+        res.render("members");
     });
 
+    app.get("/manager", function(req, res) {
+        res.render("member.handlebars");
+    });
 };
