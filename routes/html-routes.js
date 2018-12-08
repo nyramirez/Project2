c121bf115a91c6d680df8fa8bc885ebf82d31a10
Nefas <nyramirez@gmail.com>
2018-12-08 13:13:34 -0600

    making log in page, starting page and functionallity of signup page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,29 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// Requiring path to so we can use relative routes to our HTML files (Not needed any more)
+// const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-
     app.get("/", function(req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/members");
         }
-        res.render('signup');
+        res.render("login");
     });
 
-    app.get("/login", function(req, res) {
+    app.get("/signup", function(req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
             res.redirect("/members");
         }
-        res.render('login');
+        res.render("signup");
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
-        res.render('members');
+        res.render("members");
     });
-
 };
