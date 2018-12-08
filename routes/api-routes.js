dce93675772cd71863494b1c3369751b68e1dcc5
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-08 10:51:39 -0600

    updated api-routes spacing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 // Requiring our models and passport as we've configured it
-var db = require("../models");
-var passport = require("../config/passport");
+const db = require("../models");
+const passport = require("../config/passport");
 
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
@@ -10,6 +10,7 @@ module.exports = function(app) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
+    console.log(req.body);
     res.json("/members");
   });
 
